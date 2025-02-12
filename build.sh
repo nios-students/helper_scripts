cf8fbafcd6403c1eadb9ccfa81cb6cf5d1acd816
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-02-12 18:07:43 +0530

    Handle without node modules

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Assume we're already in cf pages which lands us in docs folder
 
 # cleanup
-rm -rf helper-scripts/ NEP*/ contribute.md takedown.md typography.md || true 
+rm -rf helper-scripts/ NEP*/ contribute.md takedown.md typography.md ../node_modules || true 
 
 # Clone sources
 git clone https://github.com/examdawn/helper_scripts/ helper-scripts --depth=1 # Scripts
@@ -20,4 +20,5 @@ mv helper-scripts/md/*.md . # Move the md files to current docs dir
 bash helper-scripts/gen-md.sh # Create all the folders
 
 cd .. #assume we're starting in docs folder
+npm install
 npm run docs:build # Build, it will pop up in 
