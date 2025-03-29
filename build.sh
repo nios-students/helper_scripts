b21d74c3e2237cb4107053a1b7c5896cc5fc132f
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-03-29 12:01:07 +0530

    Make it fail when build fails

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+set -e 
 # curl https://raw.githubusercontent.com/nios-students/helper_scripts/refs/heads/main/build.sh | bash
 
 # Assume we're already in cf pages which lands us in docs folder
@@ -8,7 +8,7 @@
 rm -rf helper-scripts/ wiki/ home/ contribute.md takedown.md typography.md ../node_modules .vitepress/dist || true 
 
 # Clone sources
-git clone https://github.com/nios-students/helper_scripts/ helper-scripts --depth=1 # Scripts
+git clone https://github.com/nios-students/helper_scripts/ helper-scripts --depth=1 || true # Scripts 
 #git clone https://github.com/examdawn/NEP2020_2023_BCA NEP2020/2023/BCA --depth=1 # Clone BCA
 #git clone https://github.com/examdawn/NEP2020_2024_BCA NEP2020/2024/BCA --depth=1 # Clone BCA
 #git clone https://github.com/examdawn/NEP2020_2024_BSc NEP2020/2024/BSc --depth=1 # Clone BSc
@@ -16,20 +16,20 @@ git clone https://github.com/nios-students/helper_scripts/ helper-scripts --dept
 # Copy Our course content to wiki folder
 if [[ "$1" == "SKIP_DOCS" ]]; then
   echo "Skipping cloning docs, assuming it is at docs/temp folder"
-  mv temp/{wiki,home} . && mv temp/index.md .
+  mv temp/{wiki,home} . && mv temp/index.md . || true
 else
-  git clone https://github.com/nios-students/docs -b contents temp && mv temp/{wiki,home} . && mv temp/index.md .
+  git clone https://github.com/nios-students/docs -b contents temp && mv temp/{wiki,home} . && mv temp/index.md . || true
 fi
 #git clone https://github.com/examdawn/NEP_2023_BCA NEP2020/2023/BSc-Math --depth=1
 #git clone https://github.com/examdawn/NEP_2023_BCA NEP2020/2023/BSc-Physics --depth=1
 
 
-mv helper-scripts/md/*.md . # Move the md files to current docs dir
+mv helper-scripts/md/*.md . || true # Move the md files to current docs dir
 
 #bash helper-scripts/gen-md.sh # Create all the folders
 
 cd .. #assume we're starting in docs folder
-npm install
+npm install || true
 npm run docs:build # Build, it will pop up in 
 #cp docs/helper-scripts/templates/template_headers docs/.vitepress/dist/_headers # Copy headers, https://vitepress-python-editor.netlify.app/installation#_4-set-http-headers
 
