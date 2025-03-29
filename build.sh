307f3ced0708c4855e2c838287ccb78a9bc667fe
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-03-29 11:13:55 +0530

    Try to use an array to clone repos

--- build.sh
+++ build.sh
@@ -2,16 +2,31 @@
 
 # curl https://raw.githubusercontent.com/examdawn/helper_scripts/refs/heads/main/build.sh | bash
 
+
+declare -A repos=(
+  [NEP2020_2023_BCA]="https://github.com/examdawn/NEP2020_2023_BCA NEP2020/2023/BCA"
+  [NEP2020_2024_BCA]="https://github.com/examdawn/NEP2020_2024_BCA NEP2020/2024/BCA"
+  [NEP2020_2024_BSc]="https://github.com/examdawn/NEP2020_2024_BSc NEP2020/2024/BSc"
+)
+# Check if SKIP_REPO_NAME is set. If not, set it to an empty string so the script doesn't fail if it's unset.
+: "${SKIP_REPO_NAME:=""}"
+
+
 # Assume we're already in cf pages which lands us in docs folder
 
 # cleanup
 rm -rf helper-scripts/ NEP*/ contribute.md takedown.md typography.md ../node_modules .vitepress/dist || true 
 
 # Clone sources
-git clone https://github.com/examdawn/helper_scripts/ helper-scripts --depth=1 # Scripts
-git clone https://github.com/examdawn/NEP2020_2023_BCA NEP2020/2023/BCA --depth=1 # Clone BCA
-git clone https://github.com/examdawn/NEP2020_2024_BCA NEP2020/2024/BCA --depth=1 # Clone BCA
-git clone https://github.com/examdawn/NEP2020_2024_BSc NEP2020/2024/BSc --depth=1 # Clone BSc
+for repo_name in "${!repos[@]}"; do
+  # Check if the current repo_name is in the SKIP_REPO_NAME variable
+  if [[ "$SKIP_REPO_NAME" != *"$repo_name"* ]]; then
+    echo "Cloning $repo_name..."
+    git clone ${repos[$repo_name]} --depth=1
+  else
+    echo "Skipping $repo_name because it's in SKIP_$repo_name mode"
+  fi
+done
 
 
 #git clone https://github.com/examdawn/NEP_2023_BCA NEP2020/2023/BSc-Math --depth=1
