a3371a199e0e17234a3302314a5ab0d06699d57b
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-09-15 13:41:12 +0530

    Attempt to support R

--- build-py.sh
+++ build-py.sh
@@ -6,9 +6,12 @@
 git clone https://github.com/jupyterlite/demo
 cd demo 
 
+# Patches for R support
+curl https://github.com/sounddrill31/jupyterlite-demo/commit/4034b168f2b5ea7cf9e7f66a03c821edb121acff.patch | git am -m
+
 # Install Deps
 python -m pip install -r requirements.txt --break-system-packages
-
+python -m pip install jupyterlite-webr  --break-system-packages
 
 # Build it
 cp README.md content
